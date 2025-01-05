71d41b8f06cf72e13f5cdd01b8e6890ca621bc17
Erik Bernsen <erikbernsen@icloud.com>
2025-01-05 14:08:37 +0100

    Fix typo

--- lib/aes.c
+++ lib/aes.c
@@ -21,7 +21,7 @@
 #ifdef __APPLE__
 #include "aes_apple.h"
 #else
-#include "eas_reference.h"
+#include "aes_reference.h"
 #endif
 
 void AES128_ECB_encrypt(uint8_t* input, const uint8_t* key, uint8_t *output) {
